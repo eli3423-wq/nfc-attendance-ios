a35ade1297c5efb64fd2b322abbed696bf0ec57d
eli3423-wq <eli3423@gmail.com>
2026-09-10 00:32:20 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE = "ags-attendance-v1";
+const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(()=>{}));
+  self.skipWaiting();
+});
+self.addEventListener("activate", e => e.waitUntil(self.clients.claim()));
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
